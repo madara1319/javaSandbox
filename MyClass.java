64a57d25643062fee385ac15f7d75aa86457e4b2
madara1319 <mateusz.bik@op.pl>
2024-02-08 21:54:38 +0100

    initial commit

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+public class MyClass{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
+
+public abstract class Duck{
+    QuackBehavior quackBehavior;
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+}
+
+public class MallardDuck()
+{
+    quackBehavior=new Quack();
+    flyBeahavior=new FlyWithWings();
+public void display()
+{
+    System.out.println("Im a real Mallard duck");
+}
+}
+
+
