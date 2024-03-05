fecbf3bb0bff8b80e28d4d5b66ddec09aff2b87d
madara1319 <mateusz.bik@op.pl>
2024-03-05 21:45:16 +0100

    added weather o rama app implementing observer pattern with pull method

---
+++ observerWeatherPushPull/Observer.java
@@ -0,0 +1,5 @@
+public interface Observer{
+    //state observers get from the subject when a weather measurement changes
+    public void update();
+}
+
---
+++ observerWeatherPushPull/WeatherData.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+//WeatherData implements Subject interface
+public class WeatherData implements Subject{
+//ArrayList to hold observers we create it in constructor
+    public List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData(){
+        observers = new ArrayList<Observer>();
+    }
+
+    public void registerObserver(Observer o){
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o){
+        observers.remove(o);
+    }
+   //we know all observers must implement update method so we notify them like that 
+    public void notifyObservers(){
+        for (Observer observer : observers){
+            observer.update();
+        }
+    }
+    
+//we notify everytime we get new data
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+//we dont have real measurements so we implement this method to test our display elements could take measurements from web someday tho
+    public void setMeasurements(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public float getPressure(){
+        return pressure;
+    }
+    
+    public float getHumidity(){
+        return humidity;
+    }
+    public float getTemperature(){
+        return temperature;
+    }
+//other WeatherData methods here
+    }
