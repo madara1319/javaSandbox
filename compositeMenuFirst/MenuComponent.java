eb9632a5d15fe62affa4a4130d6ab5dec1e278b6
madara1319 <mateusz.bik@op.pl>
2024-03-16 17:23:28 +0100

    added menu application using composite pattern

---
+++ compositeMenuFirst/MenuComponent.java
@@ -0,0 +1,29 @@
+public abstract class MenuComponent{
+    //grouped together composite methods to add, remove get MenComponents
+    public void add(MenuComponent menuComponent){
+        throw new UnsupportedOperationException();
+    }
+    public void remove(MenuComponent menuComponent){
+        throw new UnsupportedOperationException();
+    }
+    public MenuComponent getChild(int i){
+        throw new UnsupportedOperationException();
+    }
+//operation methods used by MenuItems some can be used in Menu too
+    public String getName(){
+        throw new UnsupportedOperationException();
+    }
+    public String getDescription(){
+        throw new UnsupportedOperationException();
+    }
+    public double getPrice(){
+        throw new UnsupportedOperationException();
+    }
+    public boolean isVegetarian(){
+        throw new UnsupportedOperationException();
+    }
+//print is operation for both Menus and MenuItems
+    public void print(){
+        throw new UnsupportedOperationException();
+    }
+}
