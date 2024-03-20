1c0a5699ab38635c0cfd3479329ad626b3397092
madara1319 <mateusz.bik@op.pl>
2024-03-20 20:15:36 +0100

    minor change

--- MVCJavaDJ/DJTestDrive.java
+++ MVCJavaDJ/DJTestDrive.java
@@ -2,5 +2,6 @@ public class DJTestDrive{
     public static void main(String[] args){
         BeatModelInterface model = new BeatModel();
         ControllerInterface controller = new BeatController(model);
+        System.out.println("test");
     }
 }
